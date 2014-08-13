28697d9fafb4fa181d1b7219a503f871d44cb83c
Martino Visintin <mvisinti@thoughtworks.com>
2014-08-13 22:17:01 +0200

    hack for packer provisioning

--- provision.sh
+++ provision.sh
@@ -2,18 +2,24 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-source './variables.sh'
+# FIXME: when invoked by packer, all relative paths won't work
+#        (it just calls this provision.sh script)
+#        the current solution (hardcoding everywhere) isn't too good
+AUTO_ARCH_PATH=/auto_arch
+
+source '$AUTO_ARCH_PATH/variables.sh'
 
 chmod +x ./provisioning/*.sh
 
-./provisioning/upgrade-system.sh
-./provisioning/packages.sh
-./provisioning/vim.sh
+$AUTO_ARCH_PATH/provisioning/upgrade-system.sh
+$AUTO_ARCH_PATH/provisioning/packages.sh
+$AUTO_ARCH_PATH/provisioning/vim.sh
 
 # FIXME: too heavy/not needed for now, re-add later
-# ./provisioning/haskell.sh
+# $AUTO_ARCH_PATH/provisioning/haskell.sh
 
-./provisioning/virtualbox-guest-utils.sh
-./provisioning/vagrant.sh
+$AUTO_ARCH_PATH/provisioning/virtualbox-guest-utils.sh
+$AUTO_ARCH_PATH/provisioning/vagrant.sh
 
-./provisioning/create-default-user.sh
+$AUTO_ARCH_PATH/provisioning/create-default-user.sh
+
