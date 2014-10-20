a58d45f8c344ba35444bd77285cfc64792998b88
Martino Visintin <mvisinti@thoughtworks.com>
2014-10-20 13:19:55 +0200

    install packer dependencies manually

--- provisioning/aur.sh
+++ provisioning/aur.sh
@@ -5,11 +5,11 @@ IFS=$'\n\t'
 AUR_BUILDS_USER=aurbuilds
 AUR_BUILDS_DIR=/tmp/aurbuilds
 
+echo "==> creating $AUR_BUILDS_USER user for building AUR packages; logging it in"
+useradd $AUR_BUILDS_USER || true
+
 echo "==> installing base-devel package group"
 pacman -S --needed --noconfirm base-devel
-
-echo "==> creating $AUR_BUILDS_USER user for building AUR packages and logging in"
-useradd $AUR_BUILDS_USER || true
 
 echo "==> fetching packer package"
 mkdir -p $AUR_BUILDS_DIR
@@ -20,8 +20,11 @@ tar -xvf packer.tar.gz && rm packer.tar.gz
 chown -R $AUR_BUILDS_USER $AUR_BUILDS_DIR
 cd packer
 
-echo "==> maing packer package (makepkg)"
-su -c "makepkg -s" $AUR_BUILDS_USER
+echo "==> installing packer dependencies"
+pacman -S --needed --noconfirm grep sed bash curl pacman jshon expac
+
+echo "==> making packer package (makepkg)"
+su -c "makepkg" $AUR_BUILDS_USER
 
 echo "==> installing packer"
 sudo pacman -U --noconfirm packer*.tar.xz
