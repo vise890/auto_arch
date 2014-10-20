6f6a0423cfaf07361e287f921ccfc9255947958e
Martino Visintin <mvisinti@thoughtworks.com>
2014-10-20 14:14:15 +0200

    refactor dependencies install

--- provisioning/aur.sh
+++ provisioning/aur.sh
@@ -14,19 +14,17 @@ pacman -S --needed --noconfirm base-devel
 echo "==> fetching packer package"
 mkdir -p $AUR_BUILDS_DIR
 cd $AUR_BUILDS_DIR
-wget https://aur.archlinux.org/packages/pa/packer/packer.tar.gz
+wget "https://aur.archlinux.org/packages/pa/packer/packer.tar.gz"
 tar -xvf packer.tar.gz && rm packer.tar.gz
-
 chown -R $AUR_BUILDS_USER $AUR_BUILDS_DIR
 cd packer
 
-echo "==> installing packer dependencies"
-pacman -S --needed --noconfirm grep sed bash curl pacman jshon expac
-
 echo "==> making packer package (makepkg)"
-su -c "makepkg" $AUR_BUILDS_USER
+makepkg --clean # cleaining dir from (eventual) previous builds
+pacman -S --needed --noconfirm git grep sed bash curl pacman jshon expac # installing packer dependencies
+su -c "makepkg" $AUR_BUILDS_USER # build the package
 
 echo "==> installing packer"
-sudo pacman -U --noconfirm packer*.tar.xz
+makepkg --install # install the package built with makepkg
 
 echo "==> have a look at /etc/makepkg.conf for more configuration options"
