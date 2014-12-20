97a39ae775d6cd508a3fb0f80235a469300e8c0d
Martino Visintin <vise890@gmail.com>
2014-12-20 17:40:25 +0100

    add extra messages and remove unnecessary steps

--- provisioning/haskell.sh
+++ provisioning/haskell.sh
@@ -5,20 +5,18 @@ IFS=$'\n\t'
 echo "==> adding haskell-core to pacman.conf"
 # FIXME: this section should go before [extra]
 # to take precedence over those packages
-echo "
-
+echo '
 [haskell-core]
-Server = http://xsounds.org/~haskell/core/\$arch
-" >> /etc/pacman.conf
+Server = http://xsounds.org/~haskell/core/$arch
+' >> /etc/pacman.conf
 
+echo "==> adding haskell-core key"
 pacman-key -r 4209170B
 # FIXME: should probably check the fingerprint
 pacman-key --lsign-key 4209170B
-pacman-key --refresh-keys
-
-pacman -Syy --noconfirm
 
 echo "==> installing haskell"
+pacman -Syy --noconfirm
 pacman -S --noconfirm ghc \
   cabal-install \
   happy alex \
