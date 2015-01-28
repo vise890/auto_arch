ef76fc1ffacfd77cdd41c25c7e41092a4bde0bf9
Marin Alcaraz <marin.alcaraz@gmail.com>
2015-01-28 21:51:08 +0000

    ignore vim tmp files

--- prepare_disk.sh
+++ prepare_disk.sh
@@ -5,9 +5,32 @@ IFS=$'\n\t'
 source './variables.sh'
 
 echo '==> preparing the disk'
+umount -R "$MAIN_MOUNTPOINT" || true
+swapoff "$SWAP_PARTITION" || true
+
 sgdisk --zap-all "$DISK"
-sgdisk --new=1:0:0 "$DISK"
-mkfs.ext4 -q "$ROOT_PARTITION"
+
+sgdisk --new=1:0:+"${ROOT_SIZE}M" "$DISK"
+
+sgdisk --new=2:0:+"${SWAP_SIZE}M" "$DISK"
+
+sgdisk --new=3:0:0 "$DISK"
+
+echo "==> PARTITION TREE CREATED"
+
+echo "==> Initializing partitions..."
+
+mkfs.ext4 "$ROOT_PARTITION"
+
+mkswap "$SWAP_PARTITION"
+echo "==> turning on swap" 
+swapon "$SWAP_PARTITION"
+
+mkfs.ext4 "$HOME_PARTITION"
 
 echo "==> mounting $ROOT_PARTITION on $MAIN_MOUNTPOINT"
 mount "$ROOT_PARTITION" "$MAIN_MOUNTPOINT"
+
+echo "==> mounting $HOME_PARTITION on $HOME_MOUNTPOINT"
+mkdir "$HOME_MOUNTPOINT"
+mount "$HOME_PARTITION" "$HOME_MOUNTPOINT"
--- variables.sh
+++ variables.sh
@@ -1,13 +1,20 @@
 export DISK='/dev/sda'
 export ROOT_PARTITION="${DISK}1"
+export SWAP_PARTITION="${DISK}2"
+export HOME_PARTITION="${DISK}3"
+
+#IN MEGABYTES or MB
+export ROOT_SIZE="15000"
+export SWAP_SIZE="512"
 
 export AUTO_ARCH_INSTALL_PATH="/auto_arch"
 export MAIN_MOUNTPOINT="/mnt"
+export HOME_MOUNTPOINT="/mnt/home"
 
-export NEW_HOSTNAME="renegade-box"
+export NEW_HOSTNAME="March-VM"
 
 export LOCALTIME="UTC"
-export LOCALE="en_GB.UTF-8"
+export LOCALE="en_US.UTF-8"
 
-export DEFAULT_USER="renegade"
-export DEFAULT_PASSWORD="renegade"
+export DEFAULT_USER="marin"
+export DEFAULT_PASSWORD="marinftw"
