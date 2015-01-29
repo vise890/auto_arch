c802fa0bc60543692adc85e0fed148dbf5d3bf84
Martino Visintin <mvisinti@thoughtworks.com>
2015-01-29 14:51:39 -0500

    fix dotfiles script

--- provisioning/dotfiles.sh
+++ provisioning/dotfiles.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 echo "==> giving $DEFAULT_USER some pimpin dotfiles"
-cd "$DEFAULT_HOME"
+cd "/home/$DEFAULT_USER"
 sudo -u "$DEFAULT_USER" git clone https://github.com/vise890/dotfiles
 cd ./dotfiles
 sudo -u "$DEFAULT_USER" ./install.sh
