6b29ccf495e1d0d88c0739ec760c9039d33b79f4
Martino Visintin <mvisinti@thoughtworks.com>
2015-02-02 18:05:56 -0500

    introduce commented out steps

--- provision.sh
+++ provision.sh
@@ -6,11 +6,14 @@ source './variables.sh'
 
 ./provisioning/upgrade-system.sh
 ./provisioning/packages.sh
+#./provisioning/haskell.sh
 ./provisioning/vim.sh
 ./provisioning/aur.sh
+#./provisioning/gnome.sh
+#./provisioning/infinality.sh
 
 ./provisioning/create-default-user.sh
-./provisioning/ssh.sh # ssh access for default user, needs authorized keys to be present
+#./provisioning/ssh.sh # ssh access for default user, needs authorized keys to be present
 
 ./provisioning/dotfiles.sh
 ./provisioning/zsh-as-default.sh
