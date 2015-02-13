a805af40316fc2e65829bfd30bdb12de988beb21
Martino Visintin <mvisinti@thoughtworks.com>
2015-02-13 18:06:50 -0500

    add ssh access for default user

--- provision.sh
+++ provision.sh
@@ -13,7 +13,7 @@ source './variables.sh'
 #./provisioning/infinality.sh
 
 ./provisioning/create-default-user.sh
-#./provisioning/ssh-for-default-user.sh # ssh access for default user, needs authorized keys to be present
+./provisioning/ssh-for-default-user.sh # ssh access for default user, needs authorized keys to be present
 
 ./provisioning/dotfiles.sh
 ./provisioning/zsh-as-default.sh
