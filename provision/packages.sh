61cb01b8f4e48e2e07c9c8a261cf6ea21e968259
Martino Visintin <vise890@gmail.com>
2015-07-14 17:20:58 +0200

    add more packages i use

--- provision/packages.sh
+++ provision/packages.sh
@@ -4,6 +4,9 @@ IFS=$'\n\t'
 
 echo "==> installing software i like"
 pacman -S --noconfirm --needed \
-    tree ranger autojump stow wget \
+    tree ranger autojump cloc \
+    stow \
+    wget weechat lynx \
+    pass \
     openssh \
     git the_silver_searcher \
