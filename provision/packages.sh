06361bd33b7086836164a2c247cc65f97886a6a4
Martino Visintin <vise890@gmail.com>
2015-08-04 13:39:58 +0200

    add system monitor apps

--- provision/packages.sh
+++ provision/packages.sh
@@ -5,6 +5,7 @@ IFS=$'\n\t'
 echo "==> installing software i like"
 pacman -S --noconfirm --needed \
     tree ranger autojump cloc \
+    htop glances python-bottle hddtemp python-matplotlib \
     stow \
     wget weechat lynx \
     pass \
