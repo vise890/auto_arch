70f8345779764e42ffdee8f1ed6b76a3e44a236f
Martino Visintin <vise890@gmail.com>
2016-01-23 15:00:29 +0000

    add pygmentize and thefuck and rearrange

--- provision/packages.sh
+++ provision/packages.sh
@@ -4,15 +4,24 @@ IFS=$'\n\t'
 
 echo "==> installing software i like"
 pacman -S --noconfirm --needed \
-    tree ranger autojump cloc \
+    zsh autojump thefuck \
+    tree ranger \
+
     vim vim-spell-en neovim \
     libqalculate \
+
     htop glances python-bottle hddtemp python-matplotlib \
+
     git tig \
     the_silver_searcher \
-    httpie \
-    aria2 rtorrent \
+    pygmentize cloc \
+
+    httpie jq \
+
     stow \
+
+    aria2 rtorrent \
     wget weechat lynx \
+
     pass \
     openssh
