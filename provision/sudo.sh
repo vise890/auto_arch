de3197fdb24e3b7fb944bd25e7f923e32159a6fe
Martino Visintin <vise890@gmail.com>
2016-01-24 18:08:51 +0000

    add moar message

--- provision/sudo.sh
+++ provision/sudo.sh
@@ -2,7 +2,9 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-echo "==> allow all members of wheel to be sudoers"
+echo "==> installing sudo"
 pacman -S --noconfirm sudo
+
+echo "==> allow all members of wheel to be sudoers"
 sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 visudo -c # abort if /etc/sudoers became corrupted
