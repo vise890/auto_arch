fedccb0b69d664c28da46845295c4c90dfd09558
Martino Visintin <vise890@gmail.com>
2016-01-24 17:49:01 +0000

    simplify haskell provisioning

--- provision/haskell.sh
+++ provision/haskell.sh
@@ -2,21 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-echo "==> adding haskell-core to pacman.conf"
-# FIXME: this section should go before [extra]
-#        to take precedence over those packages
-echo '
-[haskell-core]
-Server = http://xsounds.org/~haskell/core/$arch
-' >> /etc/pacman.conf
-
-echo "==> adding haskell-core key"
-pacman-key -r 4209170B
-# FIXME: should probably check the fingerprint
-pacman-key --lsign-key 4209170B
-
 echo "==> installing haskell"
-pacman -Syy --noconfirm
-pacman -S --noconfirm ghc \
-  cabal-install \
-  happy alex
+pacman -S --noconfirm \
+  ghc stack \
+  hlint stylish-haskell hasktags ghc-mod # dev tools
