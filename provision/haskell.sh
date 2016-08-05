fa854fa48a2af184e2624b62ba5aee81ab835fdb
Martino Visintin <vise890@gmail.com>
2016-08-05 19:41:10 +0100

    add libs needed by stack install

--- provision/haskell.sh
+++ provision/haskell.sh
@@ -6,3 +6,6 @@ echo "==> installing haskell"
 sudo pacman -S --noconfirm \
   ghc cabal-install stack \
   hlint stylish-haskell hasktags ghc-mod # dev tools
+
+echo "==> installing libs needed by 'stack setup'"
+packer -S --noconfirm ncurses5-compat-libs
