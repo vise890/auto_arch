fe2514a8aadfe8fac19fe25699204656e2779513
Martino Visintin <martino.visintin@ovoenergy.com>
2016-08-17 11:28:36 +0100

    add tiling wm goodies in official repos

--- provision/i3.sh
+++ provision/i3.sh
@@ -5,7 +5,6 @@ IFS=$'\n\t'
 echo '==> Installing i3 packages'
 sudo pacman -S --needed --noconfirm \
   i3 \
-  feh xautolock dmenu rxvt-unicode
-
-echo '==> Installing yegonesh (application launcher)'
-packer -S --noconfirm yegonesh
+  feh xautolock \
+  dmenu rofi \
+  uzbl rxvt-unicode
