8e3f53706f5cb17a3ab1ab955d366ac74706335c
Martino Visintin <martino.visintin@ovoenergy.com>
2016-08-20 14:40:50 +0100

    add more pkgs to python

--- provision/python.sh
+++ provision/python.sh
@@ -5,12 +5,18 @@ IFS=$'\n\t'
 echo '==> Installing python'
 sudo pacman -S --noconfirm --needed \
     python \
-    python-pip \
-    bpython flake8 \
-    ipython \
+    \
+    python-pip python-virtualenv \
+    \
+    python-isort python-pylint python-pycodestyle autopep8 \
+    python-jedi \
+    \
+    bpython ipython \
     jupyter jupyter-notebook python-ipykernel \
+    \
     python-numpy python-scipy \
     python-pandas python-sympy
     python-matplotlib \
     python-nose
-
+    \
+    python-requests python-jinja python-peewee python-flask
