6874fd1bb78ea8cdcd864ad1e263b2ad72b17581
Martino Visintin <vise890@gmail.com>
2016-09-17 13:38:38 +0100

    add hspec to the mix]

--- provision/haskell.sh
+++ provision/haskell.sh
@@ -5,7 +5,7 @@ IFS=$'\n\t'
 echo "==> installing haskell"
 sudo pacman -S --needed --noconfirm \
   ghc cabal-install stack \
-  hlint stylish-haskell hasktags ghc-mod # dev tools
+  hlint stylish-haskell hasktags ghc-mod hspec # dev tools
 
 echo "==> installing libs needed by 'stack setup'"
 packer -S --needed --noconfirm ncurses5-compat-libs
