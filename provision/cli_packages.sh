270e8e422a16d72a46b3feb8ee1fd9668b0f3520
Martino Visintin <martino.visintin@ovoenergy.com>
2016-09-26 12:58:57 +0100

    add ripgrep and restructure

--- provision/cli_packages.sh
+++ provision/cli_packages.sh
@@ -5,20 +5,32 @@ IFS=$'\n\t'
 echo "==> installing software i like"
 sudo pacman -S --noconfirm --needed \
     zsh zsh-syntax-highlighting \
+    \
     autojump \
     tree ranger rsync \
-    bpython \
+    \
     vim vim-spell-en neovim python-neovim python2-neovim \
+    \
+    bpython \
+    \
     mutt isync msmtp msmtp-mta \
+    \
     shellcheck \
     htop \
+    \
     git tig \
-    the_silver_searcher \
+    the_silver_searcher ripgrep \
+    \
     pygmentize cloc \
+    \
     httpie jq \
+    \
     stow \
+    \
     aria2 \
     wget lynx \
+    \
     pass \
     openssh \
+    \
     alsa-utils
