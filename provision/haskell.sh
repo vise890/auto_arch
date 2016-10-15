f3699ff1495db33e94b3d083ad6e511375887486
Martino Visintin <vise890@gmail.com>
2016-10-15 14:51:53 +0100

    remove unneeded pkgs

--- provision/haskell.sh
+++ provision/haskell.sh
@@ -6,6 +6,3 @@ echo "==> installing haskell"
 sudo pacman -S --needed --noconfirm \
   ghc cabal-install stack \
   hlint stylish-haskell hasktags ghc-mod hspec # dev tools
-
-echo "==> installing libs needed by 'stack setup'"
-packer -S --needed --noconfirm ncurses5-compat-libs
