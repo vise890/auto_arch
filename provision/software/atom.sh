87645af1d5f899ffefcd74588cb4825e7db46ad0
Martino Visintin <vise890@gmail.com>
2016-10-26 18:33:23 +0200

    simplify package only provisions

--- provision/software/atom.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
-
-echo "==> Installing atom"
-sudo pacman -S --noconfirm --needed \
-  atom apm \
-  trash-cli
-
--- provision/software/ranger.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
-
-echo "==> installing ranger and deps"
-sudo pacman -S --noconfirm --needed \
-    ranger \
-    python-chardet sudo libcaca highlight atool w3m lynx elinks poppler transmission-cli mediainfo
