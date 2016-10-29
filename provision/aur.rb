8f2757b03b063655f9c67421499c1c10701bfe87
Martino Visintin <vise890@gmail.com>
2016-10-29 15:37:30 +0200

    add xdpy

--- provision/aur.rb
+++
@@ -1,9 +0,0 @@
-#/bin/env ruby
-
-if ARGV[0]
-  target = "#{ARGV[0]}.pkgs"
-else
-  target = "*.pkgs"
-end
-
-system("pacaur -S --noconfirm --noedit --needed $(cat aur/#{target})", out: $stdout, err: $stderr)
