38761dbed819aea875360690954129a261f5589b
Martino Visintin <vise890@gmail.com>
2017-02-12 20:23:25 +0000

    fix init script

--- software/aur/fonts/init.sh
+++ software/aur/fonts/init.sh
@@ -7,23 +7,19 @@ sudo -v
 # shamelessly stolen from
 # https://gist.github.com/cryzed/e002e7057435f02cc7894b9e748c5671
 
-echo "==> installing base font stuff"
-pkgs="$(dirname "$0")/fonts.pkgs"
-sudo pacman -S --noconfirm --needed "$(cat "$pkgs")"
-
 echo "==> symlinking profiles"
 sudo ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d || true
 sudo ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d || true
 sudo ln -s /etc/fonts/conf.avail/10-hinting-slight.conf /etc/fonts/conf.d || true
 
 echo "==> installing all teh fonts"
-pacaur -S --noconfirm --needed --no-edit fonts-meta-base fonts-meta-extended-lt
+pacaur -S --noconfirm --needed --noedit fonts-meta-base fonts-meta-extended-lt
 
 echo "==> symlinking font aliases"
 sudo ln -s /etc/fonts/conf.avail/30-infinality-aliases.conf /etc/fonts/conf.d || true
 
 echo "==> adding custom ubuntu fonts profile"
-sudo cp ./90-ubuntu.conf /etc/fonts/conf.d/
+sudo cp "$(dirname $0)/90-ubuntu.conf" /etc/fonts/conf.d/
 
 echo "==> stick this into /etc/profile.d/jre.sh:"
 echo "# https://wiki.archlinux.org/index.php/java#Better_font_rendering"
